74831f8da33e85a7fe42db1083bcaf95d66dc757
Po-Hsu Lin <po-hsu.lin@canonical.com>
2020-08-07 17:39:04 +0800

    UBUNTU: SAUCE: python3 print() parentheses compatibility fix
    Add parentheses to print for python3.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- client/canonical.py
+++ client/canonical.py
@@ -7,7 +7,7 @@ import os, utils
 def setup_proxy():
     # Hacky way to use proxy settings, ideally this should be done on deployment stage
     #
-    print "Setup the http/https proxy"
+    print("Setup the http/https proxy")
     proxysets = [{'addr': 'squid.internal', 'desc': 'Running in the Canonical CI environment'},
               {'addr': '91.189.89.216', 'desc': 'Running in the Canonical enablement environment'},
               {'addr': '10.245.64.1', 'desc': 'Running in the Canonical enablement environment'}]
@@ -15,7 +15,7 @@ def setup_proxy():
         cmd = 'nc -w 2 ' + proxy['addr'] + ' 3128'
         try:
             utils.system_output(cmd, retain_output=False)
-            print proxy['desc']
+            print(proxy['desc'])
             os.environ['http_proxy'] = 'http://' + proxy['addr'] + ':3128'
             os.environ['https_proxy'] = 'http://' + proxy['addr'] + ':3128'
             break
